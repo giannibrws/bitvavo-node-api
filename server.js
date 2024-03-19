5e884b4a095c1e658b70ae70962a7f9f05804e8a
Gianni <giannibrouwers@hotmail.com>
2024-03-19 19:10:56 +0100

    + Add endpoints to create/view/ buy and sell offers

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 
 const express = require('express')
+const axios = require('axios');
+
 const ticker = require('./models/tickerModel')
 const utils = require('./utils/utils');
 const app = express()
+
+// Allow express to parse JSON bodies
+app.use(express.json())
 
 // Bitvavo config
 const bitvavo = require('bitvavo')().options({
@@ -168,7 +173,140 @@ app.get('/assets', async (req, res) => {
 //     console.log(response)
 // })
 
+
+/**
+ * TRADING SECTION
+ */
+
+
+/**
+ * Create an open order on the Bitvavo platform
+ * e.g. /create-order/VET
+ *
+ * @route POST /create-order/:symbol
+ * @param {string} :symbol.path.required - The token to buy
+ * @returns {object} 200 - 
+ * @returns {Error} 500 - Internal server error
+ */
+app.post('/create-order/:symbol', async (req, res) => {
+    let postData = {};
+
+    if (!req.params.symbol) {
+        return
+    }
+
+    try {
+        const market = req.params.symbol.toUpperCase() + "-EUR";
+
+        if (!req.body.amount) {
+            res.status(500).send('no amount');
+            return; // Halt function execution
+        }
+
+        if (!req.body.action) {
+            res.status(500).send('no action given, options = buy/sell');
+            return; // Halt function execution
+        }
+
+        // https://docs.bitvavo.com/#tag/Trading-endpoints/paths/~1order/post
+        let price = req.body.price;
+
+        // Handle lower priced tickers           
+        if (Number(price) < 1) {
+            price = parseFloat(req.body.price).toFixed(4).toString() // You can use a maximum of five digits for price
+        } else if (Number(price) > 5 && req.body.action === 'buy') {
+            res.status(500).send({
+                'error-message': 'Abort, price too high!',
+                'price': price,
+            });
+            return; // Halt function execution
+        } else {
+            price = parseFloat(req.body.price).toFixed(2).toString()
+        } 
+
+        postData = {
+            amount: req.body.amount,
+            price: price, 
+        }
+
+     // Optional parameters: limit:(amount, price, postOnly), market:(amount, amountQuote, disableMarketProtection),
+        // stopLoss/takeProfit:(amount, amountQuote, disableMarketProtection, triggerType, triggerReference, triggerAmount)
+        // stopLossLimit/takeProfitLimit:(amount, price, postOnly, triggerType, triggerReference, triggerAmount)
+        //  all orderTypes: timeInForce, selfTradePrevention, responseRequired, clientOrderId
+
+        let response = await bitvavo.placeOrder(market, req.body.action, 'limit', postData)
+        console.log(response)
+
+        res.json(response);
+    } catch (error) {
+        console.log(error)
+
+        getMininimalQuoteOrderValue(req.params.symbol.toUpperCase())
+        .then(minValue => {
+            console.log(minValue);
+    
+            res.status(500).send({
+                'error-message': "Error fetching market data " + error,
+                'symbol': req.params.symbol,
+                'request-body': postData,
+                'amountQuotes': minValue
+            });
+        })
+    }
+})
+
+/**
+ * Retrieves order information for a specific symbol and order UUID.
+ * 
+ * This endpoint fetches details for the specified order identified by its UUID.
+ * 
+ * @route GET /get-order/:symbol/:orderUuid
+ * @param {string} :symbol - The symbol for which the order details are requested.
+ * @param {string} :orderUuid - The UUID of the order for which details are requested.
+ * @returns {object} 200 - An object containing details of the requested order.
+ * @returns {object} 500 - An error message if there was an issue retrieving the order details.
+ */
+app.get('/get-order/:symbol/:orderUuid', async (req, res) => {
+    let postData = {};
+
+    if (!req.params.symbol) {
+        return
+    }
+
+    try {
+        const market = req.params.symbol.toUpperCase() + "-EUR";
+        let response = await bitvavo.getOrder(market, req.params.orderUuid)
+        console.log(response)
+
+        res.json(response);
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            'error-message': "Error retrieving order " + error,
+            'symbol': req.params.symbol,
+            'request-body': postData,
+        });
+    }
+})
+
+/**
+ * Get the minimal quote value needed to create an order on the Bitvavo platform
+ * 
+ * @param {*} symbol 
+ * @returns 
+ */
+function getMininimalQuoteOrderValue(symbol) {
+    return axios.get(process.env.BASE_URL + '/markets/' + symbol, {})
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            console.error(error);
+            return response.error;
+        });
+}
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
-    console.log('To navigate directly, go to: http://localhost:3000/')
+    console.log('To navigate directly, go to:' + process.env.BASE_URL)
 })
